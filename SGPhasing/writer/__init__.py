9a0bf125c92c7d59eb552f6ba0003fbdfec60106
sssxie <xieshang0608@gmail.com>
2021-02-21 01:58:42 +0800

    added __init__

---
+++ SGPhasing/writer/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Shang Xie.
+# All rights reserved.
+#
+# This file is part of the SGPhasing distribution and
+# governed by your choice of the "SGPhasing License Agreement"
+# or the "GNU General Public License v3.0".
+# Please see the LICENSE file that should
+# have been included as part of this package.
+"""Initialize the writer package.
+
+The SGPhasing Project is an international association of developers
+of freely available Python tools for Phasing sequences of Similar Genes.
+
+https://github.com/SGPhasing/SGPhasing
+"""
+
+__version__ = '0.0.1a'
