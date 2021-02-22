75729c4e8fb7e0fce39f73f7ed4abff5e86e1b75
sssxie <xieshang0608@gmail.com>
2021-02-22 17:19:46 +0800

    added docstring

--- SGPhasing/processor/gff_to_fasta.py
+++ SGPhasing/processor/gff_to_fasta.py
@@ -15,6 +15,16 @@ from subprocess import PIPE, Popen
 def gff_to_fasta(input_gff: str,
                  input_ref_fasta: str,
                  output_fasta: str) -> str:
+    """Convert gff3 to fasta.
+
+    Args:
+        input_gff (str): input gff3 file path str.
+        input_ref_fasta (str): input reference fasta file path str.
+        output_fasta (str): output fasta file path str.
+
+    Returns:
+        (str): gffread print.
+    """
     with Popen(['gffread', input_gff, '-g', input_ref_fasta,
                 '-w', output_fasta], stdout=PIPE) as proc:
         return proc.stdout.read().decode('utf-8').strip()
