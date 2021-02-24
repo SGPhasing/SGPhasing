2c2964572add677c7dea13e22880e287c9720c65
sssxie <xieshang0608@gmail.com>
2021-02-24 19:01:56 +0800

    changed output format

--- SGPhasing/processor/minimap2.py
+++ SGPhasing/processor/minimap2.py
@@ -12,18 +12,45 @@
 from subprocess import PIPE, Popen
 
 
-def splice_mapper(input_ref: str, input_fastx: str, threads: int):
+def splice_mapper(input_ref: str,
+                  input_fastx: str,
+                  output_sam: str,
+                  threads: int) -> str:
+    """Call minimap2 for splice mapping.
+
+    Args:
+        input_ref (str): input reference fasta file path str.
+        input_fastx (str): input sequences fasta/q file path str.
+        output_sam (str): output sam file path str.
+        threads (int): threads using for minimap2.
+
+    Returns:
+        (str): minimap2 print.
+    """
     with Popen(['minimap2', '-k', '17', '-uf', '-a', '-t', str(threads),
                 '-x', 'splice:hq', input_ref+'.mmi',
-                input_fastx], stdout=PIPE) as proc:
+                input_fastx, '>', output_sam], stdout=PIPE) as proc:
         return proc.stdout.read().decode('utf-8').strip()
 
 
 def genomic_mapper(input_ref: str,
                    input_fastx: str,
+                   output_sam: str,
                    threads: int,
-                   preset: str):
+                   preset: str) -> str:
+    """Call minimap2 for splice mapping.
+
+    Args:
+        input_ref (str): input reference fasta file path str.
+        input_fastx (str): input sequences fasta/q file path str.
+        output_sam (str): output sam file path str.
+        threads (int): threads using for minimap2.
+        preset (str): minimap2 preset.
+
+    Returns:
+        (str): minimap2 print.
+    """
     with Popen(['minimap2', '-k', '17', '-a', '-t', str(threads),
                 '-x', preset, input_ref+'.mmi',
-                input_fastx], stdout=PIPE) as proc:
+                input_fastx, '>', output_sam], stdout=PIPE) as proc:
         return proc.stdout.read().decode('utf-8').strip()
