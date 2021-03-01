ba4890630d88180ccc34c4ba8c9e012f89293a71
sssxie <xieshang0608@gmail.com>
2021-03-01 21:59:15 +0800

    added gatk4 processes

--- SGPhasing/processor/process_each_link.py
+++ SGPhasing/processor/process_each_link.py
@@ -13,35 +13,44 @@ Functions:
   - process_each_link
 """
 
+from SGPhasing.processor.gatk4 import add_or_replace_read_groups
+from SGPhasing.processor.gatk4 import create_sequence_dictionary
+from SGPhasing.processor.gatk4 import haplotype_caller
+from SGPhasing.processor.gatk4 import left_align_indels
 from SGPhasing.processor.gff_to_fasta import gff_to_fasta
 from SGPhasing.processor.minimap2 import genomic_mapper
 from SGPhasing.reader import read_fastx, read_xam
-from SGPhasing.writer import write_fastx
+from SGPhasing.writer import write_fastx, write_xam
 
 
-def process_each_link(args_tuple: tuple):
+def process_each_link(args_tuple: tuple) -> None:
     """Process each linked region.
 
     Args:
         link_id (str): linked_region id.
         linked_region (Linked_Region): linked_region.
         tmp_dir (PosixPath): temporary folder PosixPath.
-        reference (str): input reference fasta path string.
-        main_xam (str): input high quality bam/cram file.
-        main_fastx (str): input high quality fasta/q file.
-        threads (int):  threads using for minimap2, default = 1.
+        reference (str): input reference fasta file path string.
+        index_xam (str): input high quality bam/cram file for indexing.
+        index_fastx (str): input high quality fasta/q file for indexing.
+        threads (int): threads using for minimap2, default = 1.
     """
     (link_id, linked_region, tmp_dir,
-     reference, main_xam, main_fastx, threads) = args_tuple
+     reference, index_xam, index_fastx, threads) = args_tuple
     link_floder_path = tmp_dir / link_id
     if not link_floder_path.is_dir():
         link_floder_path.mkdir()
+    opened_gffread_log = (link_floder_path / 'gffread.log').open('w')
+    opened_minimap2_log = (link_floder_path / 'minimap2.log').open('w')
+    opened_gatk4_log = (link_floder_path / 'gatk4.log').open('w')
+
     link_gff_path = (
         link_floder_path / 'linked_region.minimap2_reference.gff3')
     linked_region.write_gff(str(link_gff_path))
     link_fasta_path = (
         link_floder_path / 'linked_region.reference.fasta')
-    gff_to_fasta(str(link_gff_path), reference, str(link_fasta_path))
+    opened_gffread_log.write(gff_to_fasta(
+        str(link_gff_path), reference, str(link_fasta_path)))
 
     expanded_primary_region = linked_region.extend_primary()
     expanded_primary_region.update_info_id(link_id)
@@ -52,33 +61,80 @@ def process_each_link(args_tuple: tuple):
     opened_expand_gff.close()
     expand_fasta_path = (
         link_floder_path / 'expanded_primary.reference.fasta')
-    gff_to_fasta(opened_expand_gff.name, reference, str(expand_fasta_path))
+    opened_gffread_log.write(gff_to_fasta(
+        opened_expand_gff.name, reference, str(expand_fasta_path)))
 
     link_reads_set = set()
-    opened_main_xam, main_xam_format = read_xam.open_xam(main_xam)
-    for read in opened_main_xam.fetch(linked_region.Primary_Region.chrom,
-                                      linked_region.Primary_Region.start,
-                                      linked_region.Primary_Region.end):
+    opened_index_xam, index_xam_format = read_xam.open_xam(index_xam)
+    for read in opened_index_xam.fetch(linked_region.Primary_Region.chrom,
+                                       linked_region.Primary_Region.start,
+                                       linked_region.Primary_Region.end):
         link_reads_set.add(read.query_name)
     for region in linked_region.Secondary_Regions_list:
-        for read in opened_main_xam.fetch(
+        for read in opened_index_xam.fetch(
                 region.chrom, region.start, region.end):
             link_reads_set.add(read.query_name)
-    opened_main_xam.close()
+    opened_index_xam.close()
 
-    opened_main_fastx, main_fastx_format = read_fastx.open_fastx(main_fastx)
-    main_fastx_path = (
-        link_floder_path / ('linked_region.main.' + main_fastx_format))
+    opened_index_fastx, index_fastx_format = read_fastx.open_fastx(index_fastx)
+    index_fastx_path = (
+        link_floder_path / ('linked_region.index.' + index_fastx_format))
     write_fastx.write_partial_fastx(
-        opened_main_fastx, main_fastx_path.open('w'),
-        main_fastx_format, link_reads_set)
-    opened_main_fastx.close()
+        opened_index_fastx, index_fastx_path.open('w'),
+        index_fastx_format, link_reads_set)
+    opened_index_fastx.close()
 
     link_expand_sam_path = (
         link_floder_path / 'linked_region.reference.minimap2_expand.sam')
-    genomic_mapper(str(expand_fasta_path), str(link_fasta_path),
-                   str(link_expand_sam_path), 'asm20', threads)
-    main_expand_sam_path = (
-        link_floder_path / 'linked_region.main.minimap2_expand.sam')
-    genomic_mapper(str(expand_fasta_path), str(main_fastx_path),
-                   str(main_expand_sam_path), 'asm20', threads)
+    opened_minimap2_log.write(genomic_mapper(
+        str(expand_fasta_path), str(link_fasta_path),
+        str(link_expand_sam_path), 'asm20', threads))
+    index_expand_sam_path = (
+        link_floder_path / 'linked_region.index.minimap2_expand.sam')
+    opened_minimap2_log.write(genomic_mapper(
+        str(expand_fasta_path), str(index_fastx_path),
+        str(index_expand_sam_path), 'asm20', threads))
+
+    link_expand_bam_path = (
+        link_floder_path / 'linked_region.reference.minimap2_expand.bam')
+    write_xam.sam_to_bam(str(link_expand_sam_path), str(expand_fasta_path),
+                         str(link_expand_bam_path), threads)
+    index_expand_bam_path = (
+        link_floder_path / 'linked_region.index.minimap2_expand.bam')
+    write_xam.sam_to_bam(str(index_expand_sam_path), str(expand_fasta_path),
+                         str(index_expand_bam_path), threads)
+
+    read_fastx.faidx(str(expand_fasta_path))
+    opened_gatk4_log.write(create_sequence_dictionary(str(expand_fasta_path)))
+    link_expand_group_bam_path = (
+        link_floder_path / 'linked_region.reference.minimap2_expand.group.bam')
+    opened_gatk4_log.write(add_or_replace_read_groups(
+        str(link_expand_bam_path), str(link_expand_group_bam_path),
+        'reference', 'SGPhasing', link_id, '0'))
+    index_expand_group_bam_path = (
+        link_floder_path / 'linked_region.index.minimap2_expand.group.bam')
+    opened_gatk4_log.write(add_or_replace_read_groups(
+        str(index_expand_bam_path), str(index_expand_group_bam_path),
+        'index', 'SGPhasing', link_id, '0'))
+
+    link_expand_lalign_bam_path = (
+        link_floder_path/'linked_region.reference.minimap2_expand.lalign.bam')
+    opened_gatk4_log.write(left_align_indels(
+        str(link_expand_group_bam_path), str(link_expand_lalign_bam_path),
+        str(expand_fasta_path)))
+    index_expand_lalign_bam_path = (
+        link_floder_path / 'linked_region.index.minimap2_expand.lalign.bam')
+    opened_gatk4_log.write(left_align_indels(
+        str(index_expand_group_bam_path), str(index_expand_lalign_bam_path),
+        str(expand_fasta_path)))
+
+    index_expand_vcf_path = (
+        link_floder_path / 'linked_region.index.minimap2_expand.hapcal.vcf')
+    opened_gatk4_log.write(haplotype_caller(
+        str(index_expand_lalign_bam_path), str(index_expand_vcf_path),
+        str(expand_fasta_path), len(link_reads_set), 20,
+        len(linked_region.Secondary_Regions_list)+1, threads))
+
+    opened_gffread_log.close()
+    opened_minimap2_log.close()
+    opened_gatk4_log.close()
