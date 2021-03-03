d213f5c8ae5ac8bd9467cba8799746fd2a37fc20
sssxie <xieshang0608@gmail.com>
2021-03-04 00:10:26 +0800

    :art: renamed function & variable

--- SGPhasing/writer/write_tsv.py
+++ SGPhasing/writer/write_tsv.py
@@ -10,25 +10,27 @@
 """SGPhasing.writer write tsv file.
 
 Functions:
-  - write_read_base_matrix
+  - write_reads_bases_matrix
 """
 
 
-def write_read_base_matrix(output_file: str,
-                          positions_list: list,
-                          reads_id_list: list,
-                          read_base_matrix: list) -> None:
+def write_reads_bases_matrix(output_file: str,
+                             positions_list: list,
+                             reads_id_list: list,
+                             reads_bases_matrix: list) -> None:
     """Write read base matrix in tsv file.
 
     Args:
         output_file (str): output file path string.
-        positions_list (list): 
+        positions_list (list): position list for each base.
+        reads_id_list (list): read_id list for each read.
+        reads_bases_matrix (list): bases matrix for each read.
     """
     with open(output_file, 'w') as opened_tsv:
         HEADER = [str(pos) for pos in sorted(positions_list)]
         HEADER.insert(0, '')
         opened_tsv.write('\t'.join(HEADER)+'\n')
         for read_index, read_id in enumerate(reads_id_list):
-            outline = read_base_matrix[read_index][::]
+            outline = reads_bases_matrix[read_index][::]
             outline.insert(0, read_id)
             opened_tsv.write('\t'.join(outline)+'\n')
