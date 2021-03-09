734bf5fd3a9e290e9c765796c5e84d34f018bd18
sssxie <xieshang0608@gmail.com>
2021-03-09 23:37:54 +0800

    :sparkles: added positions_list output

--- SGPhasing/writer/write_index.py
+++ SGPhasing/writer/write_index.py
@@ -24,27 +24,28 @@ from SGPhasing.reader.read_fastx import get_seq_dict
 
 def write_index(output_index: str,
                 link_id_list: list,
-                region_id_main_dict_list: list,
+                process_link_returns: list,
                 tmp_floder_path: Path) -> None:
     """Write hdf5 index file.
 
     Args:
         output_index (str): output hdf5 index file path string.
         link_id_list (list): linked regions id in list.
-        region_id_main_dict_list (list): region id as key and
-                                         [chr, start, end, matrix]
-                                         as value in list.
+        process_link_returns (list): (region_id_main_dict, positions_list)
+                                     in list.
         tmp_floder_path (Path): temporary folder Path.
     """
     opened_index = File(output_index, 'w')
-    for link_id, region_id_main_dict in zip(
-            link_id_list, region_id_main_dict_list):
+    for link_id, link_tuple in zip(link_id_list, process_link_returns):
+        region_id_main_dict, positions_list = link_tuple
         link_group = opened_index.create_group(link_id)
         link_fasta_path = (
             tmp_floder_path / link_id / 'expanded_primary.reference.fasta')
         link_id_seq_dict = get_seq_dict(str(link_fasta_path))
         link_group.create_dataset(
             'reference', data=seq_to_array(link_id_seq_dict.get(link_id, '')))
+        link_group.create_dataset(
+            'positions', data=np.array(positions_list, dtype=np.uint32))
         for region_id, region_main_list in region_id_main_dict.items():
             chrom, start, end, matrix = region_main_list
             region_group = link_group.create_group(region_id)
@@ -52,7 +53,7 @@ def write_index(output_index: str,
             region_group.attrs.create('start', start)
             region_group.attrs.create('end', end)
             region_group.create_dataset('index',
-                                        data=np.array(matrix, np.int8))
+                                        data=np.array(matrix, np.uint8))
     opened_index.close()
 
 
@@ -81,4 +82,4 @@ def seq_to_array(sequence: str) -> np.ndarray:
         'D': 12, 'd': 12,
         'B': 13, 'b': 13,
         'N': 14, 'n': 14}
-    return np.array([BASE_INT[base] for base in sequence], dtype=np.int8)
+    return np.array([BASE_INT[base] for base in sequence], dtype=np.uint8)
